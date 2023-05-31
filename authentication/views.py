5462406fd0dd8da0beddf3394929ba79e8927946
lagrandecode <seunogunmolufirst1@gmail.com>
2023-05-31 01:35:07 +0100

    "commit"

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import APIView
-from rest_framework import status,generics
-from . import serializers
-from django.core.mail import send_mail
 import random
+
+from django.core.mail import send_mail
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from django.conf import settings
+
+from . import serializers
 from .models import User
 
 # Create your views here.
