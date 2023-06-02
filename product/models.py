f8ff46e103592dbc13976da14c352e1304f5cd89
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-03 00:48:40 +0100

    "commit"

--- product/models.py
+++ product/models.py
@@ -4,10 +4,18 @@ from django.db import models
 # Create your models here.
 
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+
+
 class Product(models,Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=500)
     product_image = models.ImageField(upload_to='photos/product_image/',blank=True,null=True)
     price = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
     description = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto)
     def __str__(self):
