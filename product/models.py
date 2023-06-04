61a5754c54387b00fe819965939552c062b2f5b1
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-05 00:18:54 +0100

    "commit"

--- product/models.py
+++ product/models.py
@@ -3,23 +3,22 @@ from django.db import models
 
 # Create your models here.
 
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField(auto_now_add=True)
-
-
 class Product(models.Model):
+    HOME_EQUIPMENT = 'Home & Garden'
+    CLOTHING = 'Clothing'
+    ACCESSORIES = 'Accessories'
+    ELECTRONICS = 'Electronics'
+    SHOES = 'Shoes'
+    JEWELRIES = 'Jewelries'
     CATEGORY = (
-        ('Home & Garden','Home & Garden'),
-        ('Clothing','Clothing'),
-        ('Accessories','Accessories'),
-        ('Electronics','Electronics'),
-        ('Shoes','Shoes'),
-        ('Jewelries','Jewelries')
+        (HOME_EQUIPMENT,'Home & Garden'),
+        (CLOTHING,'Clothing'),
+        (ACCESSORIES,'Accessories'),
+        (ELECTRONICS,'Electronics'),
+        (SHOES,'Shoes'),
+        (JEWELRIES,'Jewelries')
     )
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50,choices=CATEGORY,default=HOME_EQUIPMENT)
     name = models.CharField(max_length=500)
     product_image = models.ImageField(upload_to='photos/product_image/',blank=True,null=True)
     price = models.PositiveIntegerField()
