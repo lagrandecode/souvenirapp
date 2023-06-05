ba88df5d74bdd38d4000162708ee0fdfa7ed8f04
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-05 21:53:10 +0100

    "commit"

--- order/models.py
+++ order/models.py
@@ -4,9 +4,9 @@ from souvenirapp.product import Product
 # Create your models here.
 
 
-class Customer(models.Models):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    pro_pics = models.ImageField(upload_to='photos/customer_photo/',null=True,blank=True)
+# class Customer(models.Models):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     pro_pics = models.ImageField(upload_to='photos/customer_photo/',null=True,blank=True)
 
 
 class Orders(models.Model):
@@ -16,12 +16,15 @@ class Orders(models.Model):
         ('Out for Delivery','Out for Delivery'),
         ('Delivered','Delivered'),
     )
-    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    customer = models.OneToOneField(User, on_delete=models.CASCADE)
     product=models.ForeignKey('Product',on_delete=models.CASCADE,null=True)
     email = models.EmailField(max_length=80,null=True)
     address = models.CharField(max_length=500,null=True)
     phone_number = models.CharField(max_length=14,null=True)
     order_date= models.DateField(auto_now_add=True,null=True)
     status=models.CharField(max_length=50,null=True,choices=STATUS)
+
+    class Meta:
+        verbose_name_plural = 'Orders'
 
 
--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,4 @@
-from rest_framework import generics,status
-from rest_framework.response import Response
+
 from .models import Product
 from rest_framework import serializers
 
