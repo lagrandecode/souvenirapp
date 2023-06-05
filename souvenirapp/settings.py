dff273529150c8427b3bb689df9a44e4ffe5c658
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-05 19:33:42 +0100

    "commit"

--- souvenirapp/settings.py
+++ souvenirapp/settings.py
@@ -83,13 +83,20 @@ WSGI_APPLICATION = 'souvenirapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sample',
+        'NAME': 'djangoapp',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
