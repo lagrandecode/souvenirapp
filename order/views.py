3d1c631bbbe1481fa9f075770d4a7d3182adaa9d
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-07 19:33:54 +0100

    "commit"

--- order/views.py
+++ order/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, status 
+from rest_framework.response import Response 
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from . import serializers
+from .models import Orders
 
 # Create your views here.
+
+
+class OrderView(generics.GenericAPIView):
+    serializer_class = Orders.OrderSerializers
+    queryset = Orders.objects.all()
+    def get(self,request):
+        pass
+
+
+    def post(self,request):
+        pass
