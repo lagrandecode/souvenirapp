8dd70216ba342044901a7e371173e0ef654746db
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-08 20:47:04 +0100

    "commit"

--- order/views.py
+++ order/views.py
@@ -12,8 +12,19 @@ class OrderView(generics.GenericAPIView):
     serializer_class = Orders.OrderSerializers
     queryset = Orders.objects.all()
     def get(self,request):
-        pass
-
+        try:
+            order = Orders.objects.all()
+            serializers = self.serializer_class(order,many=True)
+            return Response(serializers.data,status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
 
     def post(self,request):
-        pass
+        try:
+            serializers = self.serializer_class(data=request.data)
+            if serializers.is_valid():
+                serializers.lsave()
+                return Response(serializers.data,status=status.HTTP_200_OK)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
