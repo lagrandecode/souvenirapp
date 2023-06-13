fe5353719bdb8d69d32a54cbe24ad049b831277c
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-13 22:45:46 +0100

    "commit"

--- feedback/views.py
+++ feedback/views.py
@@ -22,4 +22,4 @@ class FeedbackView(generics.GenericAPIView):
             return Response(serializers.data,status=status.HTTP_200_OK)
         return Response(seriliazers.errors,status=status.HTTP_400_BAD_REQUEST)
 
-        # 
+        
